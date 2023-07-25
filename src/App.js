35e0db6b146220f6d700bd47bf9e0d56cefec0e7
naomijustin <naomi.z.justin@gmail.com>
2023-07-26 07:07:39 +1000

    rendering meme text in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header.js';
+import Meme from './components/Meme.js'
 
 function App() {
   return (
     <div>
       <Header />
+      <Meme />
     </div>
   );
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Meme () {
+    return (
+        <div>
+            Test meme
+        </div>
+    )
+}
