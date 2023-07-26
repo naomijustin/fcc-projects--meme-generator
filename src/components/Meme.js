a65e0275bdb76259601332c5bd94082ade324dcb
naomijustin <naomi.z.justin@gmail.com>
2023-07-27 08:03:07 +1000

    added framed picture icon to button

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,4 +1,5 @@
 import React from "react";
+import framedPicture from "../images/framed-picture.png"
 
 export default function Meme () {
     return (
@@ -7,7 +8,7 @@ export default function Meme () {
                 <input type="text"/>
                 <input type="text"/>
             </div>
-            <button>Get a new meme image</button>
+            <button>Get a new meme image <img src={framedPicture} className="icon-Framedpicture"/></button>
             
         </section>
     )
