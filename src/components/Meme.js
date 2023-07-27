82cc2685564fb25e634d78b90758a3fcc3b0701d
naomijustin <naomi.z.justin@gmail.com>
2023-07-28 07:12:24 +1000

    Final styling before events

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,13 +3,14 @@ import framedPicture from "../images/framed-picture.png"
 
 export default function Meme () {
     return (
-        <section className="section-meme">
-            <div className="input-wrapper">
-                <input type="text"/>
-                <input type="text"/>
-            </div>
-            <button className="btn-GetNewMeme">Get a new meme image <img src={framedPicture} className="icon-Framedpicture"/></button>
-            
-        </section>
+        <main>
+            <form className="section-meme">
+                <div className="input-wrapper">
+                    <input type="text" placeholder="Top Text"/>
+                    <input type="text" placeholder="Bottom Text"/>
+                </div>
+                <button className="btn-GetNewMeme">Get a new meme image <img src={framedPicture} className="icon-Framedpicture"/></button>
+            </form>
+        </main>
     )
 }
